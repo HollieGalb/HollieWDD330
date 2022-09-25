171ce70635212d77c6dd664c293cf8db551d0ebd
Hollie Galbraith <hollie.galbraith@gmail.com>
2022-09-24 19:15:55 -0600

    week2 Notes2

--- Week2/wk2exmple.js
+++ Week2/wk2exmple.js
@@ -2,3 +2,11 @@ const question = "What is Superman's real name?"
 const answer = prompt(question);
 alert('You answered ${answer}');
 
+
+const romanNumerals = new Map();
+romanNumerals.set(1,'I');
+romanNumerals.set(2,'II').set(3,'III');
+
+romanNumerals.size;
+
+romanNumerals.delete('1');
