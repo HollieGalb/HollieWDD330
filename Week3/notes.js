7d42ec351b23bc47a88003321bfb79c57cd242ee
Hollie Galbraith <hollie.galbraith@gmail.com>
2022-10-01 16:01:51 -0600

    week3 update

---
+++ Week3/notes.js
@@ -0,0 +1,14 @@
+// . matches any character, except line breaks
+
+// \w matches any word character, and is equivalent to [A-Za-z0-9_]
+
+// \W matches any non-word character, and is equivalent to [\^A-Za-z0-9_]
+
+// \d matches any digit character, and is equivalent to [0-9]
+
+// \D matches any non-digit character, and is equivalent to [^0-9]
+
+// \s matches any whitespace character, and is equivalent to [ \t\r\n\f]
+
+// \S matches any non-whitespace character, and is equivalent to [^ \t\r\n\f]
+
